612c5b15dd4562c20e3257477f92c01c473ff844
Eric Cauble <caublee@email.uscupstate.edu>
2015-05-07 16:40:19 -0400

    removed font awesome library

--- Food Group/Food Group-Bridging-Header.h
+++ Food Group/Food Group-Bridging-Header.h
@@ -1,10 +1,9 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
+#pragma once
 #import <UIKit/UIKit.h>
 #import <Bolts/Bolts.h>
-
 #import <Foundation/Foundation.h>
 #import <ParseUI/ParseUI.h>
 #import <FacebookSDK/FacebookSDK.h>
@@ -15,6 +14,3 @@
 #import "APSocialProfile.h"
 #import "APAddress.h"
 #import "APPhoneWithLabel.h"
-#pragma once
-#import "FontAwesomeKit.h"
- 
