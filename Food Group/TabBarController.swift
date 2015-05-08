059ef395fcf7adeb72a3352488d4fdf9dc684842
Eric Cauble <caublee@email.uscupstate.edu>
2015-05-07 20:34:43 -0400

    update

--- Food Group/TabBarController.swift
+++ Food Group/TabBarController.swift
@@ -16,5 +16,8 @@ public class TabBarController: UITabBarController{
     (self.tabBar.items![1] as! UITabBarItem).selectedImage = UIImage(named: "895-user-group-selected")
     (self.tabBar.items![2] as! UITabBarItem).selectedImage = UIImage(named: "722-location-pin-selected")
     (self.tabBar.items![3] as! UITabBarItem).selectedImage = UIImage(named: "851-calendar-selected")
+        
+        self.tabBar.backgroundColor = UIColor.redColor()
+        
     }
 }
